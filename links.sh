2e62980efb346c9b1bfb925470a320995953bfe1
Craig <craig@stembolt.com>
2019-02-12 12:56:38 -0800

    add links

---
+++ links.sh
@@ -0,0 +1,4 @@
+ln -sf $HOME/.dotfiles/vim/vimrc.symlink $HOME/.vimrc
+ln -s ~/.dotfiles/git/gitconfig.symlink ~/.gitconfig
+ln -s ~/.dotfiles/tmux/tmux.conf.symlink .tmux.conf
+ln -s ~/.dotfiles/tmux .tmux
